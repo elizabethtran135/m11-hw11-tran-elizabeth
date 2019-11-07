6453657df9bf7d6f2a7809ec3590a2ca3a9ea1e3
Elizabeth Tran <elizabethtran135@gmail.com>
2019-11-06 20:42:00 -0500

    Add .js code and css transition

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 //Global Varibales
-
+const myBoxes = document.getElementsByClassName('block');
 
 //The Functionality
+function loopThrough() {
+  const changeColor = () => this.classList.toggle('red');
 
+//*2 on off
+  for(let i = 0; i < this.textContent.length * 2; i++) { setTimeout(changeColor, i * 1000);
+
+  };
+};
 
 //The Event Listener
+for (let i=0; i < myBoxes.length; i++) {
+  myBoxes[i].addEventListener('click', loopThrough);
+};
